80490648a874c78808e90cd9ec4f3b8c46587eb9
gmlwns7412 <gmlwns7412@naver.com>
2021-01-29 04:22:14 +0900

    설문조사지 js틀설정

---
+++ mbti.js
@@ -0,0 +1,12 @@
+function radio_chk(){
+   const radio_btn = document.getElementsByName("1");
+   
+   const radio_btn_check =0;
+   for(let i=0 ; i<radio_btn.length;i++);{
+
+    if(radio_btn[i].checked==true){
+        
+    }
+   }
+    
+}
