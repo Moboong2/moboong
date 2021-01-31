9ea37e2c354b47bfef00cc1b1c8cb477b45a3c07
gmlwns7412 <gmlwns7412@naver.com>
2021-02-01 03:19:07 +0900

    결과페이지 보이도록수정했으나 결과버튼이 두개나나옴... 내일확인필요...

--- mbti.js
+++ mbti.js
@@ -1,12 +1,53 @@
-function radio_chk(){
-   const radio_btn = document.getElementsByName("1");
-   
-   const radio_btn_check =0;
-   for(let i=0 ; i<radio_btn.length;i++);{
 
-    if(radio_btn[i].checked==true){
-        
+let totalpoint = 0;
+
+function value_check() {
+    let total= 0;
+    let Qnum=11;
+    let kkk = 1 ; 
+    
+   
+   
+    for(let k=1; k<Qnum;k++){
+        let check_count = document.getElementsByName(k).length;
+        for (let i=0; i<check_count; i++) {
+        if (document.getElementsByName(k)[i].checked == true) {
+         total+=Number(document.getElementsByName(k)[i].value);
+        }
     }
-   }
+   
+    }
+    
+   
+    
+      
+       if(total>=10&& total<=20){
+        $("#main-button").click(function(){ 
+            var tag = "<input type='button' id='result-button' "+'onclick='+'"'+"location.href="+"'mbti1.html' "+'"'+" value='결과!' />";
+            
+            $("#test").append(tag);
+         });
+         
+       }
+       else if(total>=21 && total<=30){
+        $("#main-button").click(function(){ 
+            var tag = "<input type='button' id='result-button' "+'onclick='+'"'+"location.href="+"'mbti2.html' "+'"'+" value='결과!' />";
+            
+            $("#test").append(tag);
+         });
+
+       }
+       else if(total>=31 && total<=40){
+        $("#main-button").click(function(){ 
+            var tag = "<input type='button' id='result-button' "+'onclick='+'"'+"location.href="+"'mbti3.html' "+'"'+" value='결과!' />";
+            
+            $("#test").append(tag);
+         });
+
+       }
+    
     
 }
+
+
+
