f42e86b57a1769173c5570cd994395f57a257311
AzureAD\황준호 <12132139@inha.edu>
2021-02-09 15:47:37 +0900

    All삭제

--- lib/delete_process.php
+++
@@ -1,4 +0,0 @@
-<?php
-unlink('./data/sample'.basename($_POST['review-id']).'/review'.'/'.basename($_POST['review-title'])); //부모디렉토리의 다른 파일이 삭제 될수도 있기에.
-header('Location: /detailpage.php?id='.basename($_POST['review-id']));
-?>
